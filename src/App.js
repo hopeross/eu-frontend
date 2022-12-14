9dbf59e4bd54e8f467e9d783e6b1d4be733470a2
Hope Ross <hope.ross@daveramsey.com>
2022-12-14 09:25:21 -0600

    yay

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+	constructor(props) {
+		super(props);
 
+		this.state = {
+			items: [],
+			DataisLoaded: false
+		};
+	}
+
+	componentDidMount() {
+		fetch("https://localhost:7284/api/barn")
+			.then((res) => res.json())
+			.then((json) => {
+				this.setState({
+					items: json,
+					DataisLoaded: true
+				});
+			})
+	}
+
+	render() {
+		const { DataisLoaded, items } = this.state;
+		if (!DataisLoaded) return <div>
+			<h1> Waiting waiting waiting </h1> </div> ;
+
+		return (
+		<div className = "App">
+			<h1> Fetch data from an api in react </h1> {
+				items.map((item) => (
+				<ol key = { item.barnId } >
+					barnname: { item.barnName }
+					</ol>
+				))
+			}
+		</div>
+	);
+	}
+	
+}
 export default App;
